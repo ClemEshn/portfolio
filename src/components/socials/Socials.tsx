f0affad7522c9fa44a28ebf64d1485dac0a478ca
LeKlem <contact.klems@gmail.com>
2025-02-03 15:12:41 +0100

    finished social links

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -1,9 +1,9 @@
 export default function Socials(){
     return (
         <div className="socials"> 
-            <a href="https://www.linkedin.com/in/clement-eischen-3355281b3/"><img className="logo" src="/logo/linkedin.svg"/></a>
-            <a href=""><img className="logo" src="/logo/github.svg"/></a>
-            <a href=""><img className="logo" src="/logo/mail.svg"/></a>
+            <a href="https://www.linkedin.com/in/clement-eischen-3355281b3/" target="_blank"><img className="logo" src="/logo/linkedin.svg"/></a>
+            <a href="https://github.com/ClemEshn" target="_blank"><img className="logo" src="/logo/github.svg"/></a>
+            <a href="mailTo:eischen.clement@gmail.com" target="_blank"><img className="logo" src="/logo/mail.svg"/></a>
 
         </div>
     )
